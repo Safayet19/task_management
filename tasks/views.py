6fae71df2a8d35a870a518cb6bf1bb82b4136eb9
Safayet Ullah <sojibmirzabd@gmail.com>
2025-07-07 21:35:36 +0600

    context and other tenmplate

--- tasks/views.py
+++ tasks/views.py
@@ -9,4 +9,8 @@ def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
 def test(request):
-    return render(request,'test.html')
+    context = {
+        "names" : ["Safayet", "Mirza", "OKOL"],
+        "age" : 23
+    }
+    return render(request,'test.html',context)
