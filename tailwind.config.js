6785162577d774f5fe65b179e182808dd2224ee1
Safayet Ullah <sojibmirzabd@gmail.com>
2025-07-07 21:06:05 +0600

    Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", //root
+    ".//templates/**/*.html"//in apps
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
